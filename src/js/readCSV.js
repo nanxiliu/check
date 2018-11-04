dc3ab424ae76c8b4dafc5851b402dcac050c93b4
Nanxi Liu <nanxi@mit.edu>
2018-11-04 01:08:12 -0700

    Dynamic csv button generating

--- src/js/readCSV.js
+++ src/js/readCSV.js
@@ -20,18 +20,42 @@ function processData(csv) {
         var currentline=lines[i].split(",");
 
         for(var j=0;j<headers.length;j++){
-            obj[headers[j]] = currentline[j];
+            obj[headers[j].replace("\r", "")] = currentline[j];
         }
         result.push(obj);
     }
-    let json = JSON.stringify(result); 
-    
     let resultsDiv = document.querySelector("#results-col");
-    let point1 = document.createElement("p");
-    let text = document.createTextNode(json);
-    point1.append(text);
-    resultsDiv.append(point1);
 
+    for(var i=0; i<result.length; i++){
+        let para1 = document.createElement("button");
+        para1.className = "btn btn" + i.toString();
+        let para2 = document.createElement("p");
+        para2.className = "para" + i.toString();
+        let candidateText = document.createTextNode(result[i]["Candidates"]);
+        let prioritiesText = document.createTextNode(result[i]["Priorities"]);
+        para1.append(candidateText);
+        resultsDiv.append(para1);
+        para2.append(prioritiesText);
+        resultsDiv.append(para2);
+    }
+
+    $(".btn0").click(function(){
+        var x = document.querySelector(".para0");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    });
+
+    $(".btn1").click(function(){
+        var x = document.querySelector(".para1");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    });
 };
 
 window.onload = function() {
